209093e860d890cfa1ceb9eb7ba903fe982a56ad
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-11-21 17:02:26 +0100

    chore: hide splashscreen

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigator from './src/navigation/MainNav';
 import {Provider} from 'react-redux';
 import store from './src/store/store';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <Navigator />
