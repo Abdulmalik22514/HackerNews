636dc05c49232788a94480fad585910d66c0d07a
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-11-25 00:06:59 +0100

    refactor: change style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import {Text, TouchableOpacity, View} from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
 import {BackArrow} from '../assets/svg/svg';
 
-export const Header = ({title, onPress}) => {
+export const Header = ({title, style}) => {
   const {pop} = useNavigation();
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       <TouchableOpacity onPress={() => pop()} hitSlop={15} activeOpacity={0.8}>
         <BackArrow />
       </TouchableOpacity>
@@ -20,13 +20,13 @@ const styles = ScaledSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: '20@s',
+    paddingLeft: '10@s',
+    paddingVertical: '10@s',
   },
   title: {
     fontSize: '20@s',
     textAlign: 'center',
-    marginLeft: '10@s',
+    marginLeft: '15@s',
     fontFamily: 'Roboto-Medium',
-    width: '90%',
   },
 });
